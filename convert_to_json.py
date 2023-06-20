ecc12fd36423d756c9f5dc0de53adbe29eac1a31
FabianWild <Fabian.Wild1996@gmx.de>
2023-06-20 19:16:57 +0200

    qqq

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,23 +1,15 @@
 import geopandas as gpd
-import json
 
-def convert_shp_to_jsonp(shp_file, jsonp_file, callback_function):
+def convert_shp_to_geojson(shp_file, geojson_file):
     # Read the shapefile
     gdf = gpd.read_file(shp_file)
 
-    # Convert to JSON
-    json_data = gdf.to_json()
-
-    # Wrap JSON data with the callback function
-    jsonp_data = f"{callback_function}({json_data});"
-
-    # Write JSONP to file
-    with open(jsonp_file, 'w') as f:
-        f.write(jsonp_data)
+    # Convert to GeoJSON
+    gdf.to_file(geojson_file, driver='GeoJSON')
 
 # Example usage
 shp_file_path = 'C:/Permafrost/Permafrost/Shapefile/IRYP_v2_yedoma_domain.shp'
-jsonp_file_path = 'C:/Permafrost/Permafrost/Shapefile/IRYP_v2_yedoma_domain.jsonp'
-callback_function_name = 'myCallbackFunction'
+geojson_file_path = 'C:/Permafrost/Permafrost/Shapefile/IRYP_v2_yedoma_domain.geojson'
 
-convert_shp_to_jsonp(shp_file_path, jsonp_file_path, callback_function_name)
+convert_shp_to_geojson(shp_file_path, geojson_file_path)
+
